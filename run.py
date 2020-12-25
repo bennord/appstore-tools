2fa8fcadc54864be6721666ca1fd528f205792e6
bennord <ben.nordstrom@gengo.com>
2020-12-25 17:54:00 +0900

    more api tests

--- run.py
+++ run.py
@@ -28,6 +28,19 @@ if verbose:
 
 # https://apps.apple.com/us/app/lionbridge-ai/id1534701695#?platform=iphone
 app_id = 1534701695
+localization_id = "2d4ae30d-7227-4f25-99e8-fca502d1844"
+
+appstore.fetch(
+    path=f"/apps/{app_id}",
+    method="get",
+    access_token=access_token,
+    verbose=verbose)
+
+appstore.fetch(
+    path=f"/apps/{app_id}?fields=bundleId",
+    method="get",
+    access_token=access_token,
+    verbose=verbose)
 
 appstore.fetch(
     path=f"/apps/{app_id}/appInfos",
@@ -36,7 +49,19 @@ appstore.fetch(
     verbose=verbose)
 
 appstore.fetch(
-    path=f"/appScreenshotSets/{app_id}/appScreenshots",
+    path=f"/apps/{app_id}/appInfos?include=appInfoLocalizations",
     method="get",
     access_token=access_token,
     verbose=verbose)
+
+appstore.fetch(
+    path=f"/appStoreVersionLocalizations/{localization_id}/appScreenshotSets",
+    method="get",
+    access_token=access_token,
+    verbose=verbose)
+
+# appstore.fetch(
+#     path=f"/appScreenshotSets/{app_id}/appScreenshots",
+#     method="get",
+#     access_token=access_token,
+#     verbose=verbose)
