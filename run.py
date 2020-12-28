3f09b08aa6fd173527a388760546389cc40f2088
bennord <ben.nordstrom@gengo.com>
2020-12-28 22:19:21 +0900

    grab loc set ID from result

--- run.py
+++ run.py
@@ -31,35 +31,50 @@ if verbose:
 app_id = 1534701695
 localization_id = "2d4ae30d-7227-4f25-99e8-fca502d1844"
 
-appstore.fetch(
-    path=f"/apps/{app_id}",
+# appstore.fetch(
+#     path=f"/apps/{app_id}",
+#     method="get",
+#     access_token=access_token,
+#     verbose=verbose)
+
+# appstore.fetch(
+#     path=f"/apps/{app_id}?fields=bundleId",
+#     method="get",
+#     access_token=access_token,
+#     verbose=verbose)
+
+# appstore.fetch(
+#     path=f"/apps/{app_id}/appInfos",
+#     method="get",
+#     access_token=access_token,
+#     verbose=verbose)
+
+# appstore.fetch(
+#     path=f"/apps/{app_id}/appInfos?include=appInfoLocalizations",
+#     method="get",
+#     access_token=access_token,
+#     verbose=verbose)
+
+# appstore.fetch(
+#     path=f"/apps/{app_id}/appStoreVersions",
+#     method="get",
+#     access_token=access_token,
+#     verbose=verbose)
+
+appstoreversion_id = "f013a7f5-46e4-4fc4-a377-27de199dee7b"
+localizations_result = appstore.fetch(
+    path=f"/appStoreVersions/{appstoreversion_id}/appStoreVersionLocalizations",
     method="get",
     access_token=access_token,
     verbose=verbose)
 
+appstoreversionlocalization_id = localizations_result["data"][0]["id"]
 appstore.fetch(
-    path=f"/apps/{app_id}?fields=bundleId",
+    path=f"/appStoreVersionLocalizations/{appstoreversionlocalization_id}/appScreenshotSets",
     method="get",
     access_token=access_token,
     verbose=verbose)
 
-appstore.fetch(
-    path=f"/apps/{app_id}/appInfos",
-    method="get",
-    access_token=access_token,
-    verbose=verbose)
-
-appstore.fetch(
-    path=f"/apps/{app_id}/appInfos?include=appInfoLocalizations",
-    method="get",
-    access_token=access_token,
-    verbose=verbose)
-
-appstore.fetch(
-    path=f"/appStoreVersionLocalizations/{localization_id}/appScreenshotSets",
-    method="get",
-    access_token=access_token,
-    verbose=verbose)
 
 # appstore.fetch(
 #     path=f"/appScreenshotSets/{app_id}/appScreenshots",
