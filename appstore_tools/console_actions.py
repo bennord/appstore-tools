86febf8792860ef323f2795536202561dd19cd87
bennord <bennord@gmail.com>
2022-11-06 00:05:20 +0900

    add timeout ocnsole param for preview updates

--- appstore_tools/console_actions.py
+++ appstore_tools/console_actions.py
@@ -143,6 +143,7 @@ def publish(args):
         platform=args.platform,
         version_string=args.version_string or args.created_version_string,
         update_version_string=args.version_string is not None,
+        media_completion_timeout_secs=args.media_completion_timeout,
         asset_ignore=args.asset_ignore,
         allow_create_version=not args.no_create_version,
         allow_create_locale=not args.no_create_locale,
